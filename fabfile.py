27d6837b58236ad67312c8197b68e56498cb5b63
Pawel Kowalski <paweloque@gmail.com>
2013-03-08 10:44:45 +0100

    use ssh config for fabric deployment

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,14 @@ import os
 from fabric.api import *
 from fabric.contrib.project import *
 
+
 def _local_path(*args):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), *args)
 
+
 # environments
-env.user = 'root'
+env.use_ssh_config = True
+env.hosts = ['cloudsigma.iterativ.ch']
 env.rsync_exclude = ['.settings/',
                      '.project',
                      '.pydevproject',
@@ -17,17 +20,16 @@ env.rsync_exclude = ['.settings/',
 env.remote_app = '/srv/www/error-pages'
 env.local_app = _local_path() + '/'
 
+
 def deploy():
 
-    run('mkdir -p %(remote_app)s' % env) 
-    
-    # sources & templates
+    sudo('mkdir -p %(remote_app)s' % env)
     rsync_project(
-        remote_dir = env.remote_app,
-        local_dir = env.local_app,
-        exclude = env.rsync_exclude,
+        remote_dir=env.remote_app,
+        local_dir=env.local_app,
+        exclude=env.rsync_exclude,
+        extra_opts='--rsync-path="sudo rsync"'
     )    
-    
-    run('/etc/init.d/nginx restart')
-    
 
+    # don't restart on error pages update
+    #run('/etc/init.d/nginx restart')
